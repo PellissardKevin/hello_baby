6ec49cafe1905f0e22a56659b53d5a80e18d3ee1
Kevin.P <kevin.pellissard@gmail.com>
2024-01-11 14:39:28 +0100

    update main.py with some check number method

--- main.py
+++ main.py
@@ -4,17 +4,31 @@ import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
+import random
+
 kivy.require('2.0.0')
 
 class GameView(BoxLayout):
     def __init__(self):
         super(GameView, self).__init__()
+        self.randomValue = random.randint(0, 1000)
 
+    def check_number(self):
+        if int(self.anwer_input.text) == self.randomValue:
+            self.result_label.text = "Congrat !"
+            self.result_label.color = "#00EF0B"
+            self.randomValue = random.randint(0, 1000)
+        elif int(self.anwer_input.text) > self.randomValue:
+            self.result_label.text = "Less !"
+            self.result_label.color = "#EF3E00"
+        elif int(self.anwer_input.text) < self.randomValue:
+            self.result_label.text = "More !"
+            self.result_label.color = "#EF3E00"
 
-class testApp(App):
+class TestApp(App):
     def build(self):
         return GameView()
 
-
-testApp = testApp()
-testApp.run()
+if __name__ == '__main__':
+    app = TestApp()
+    app.run()
