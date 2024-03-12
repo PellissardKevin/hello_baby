d620cc3a15fdad75d7199b3b7f3218a998724f93
Kevin.P <kevin.pellissard@gmail.com>
2024-03-12 19:37:56 +0100

    Update all route for go back on home. Update home baby and home user for having dynamic list

--- client/home_baby/home_baby.py
+++ client/home_baby/home_baby.py
@@ -4,6 +4,10 @@ import kivy
 from kivy.core.window import Window
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from client.login.Login import AppState
+from client.home_user.home_user import Home_user
+from client.home_baby.home_baby import Home_baby
+from client.profil_user.profil import Profil
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 
@@ -16,45 +20,50 @@ class Home_baby(Screen):
     def __init__(self, **kwargs):
         super(Home_baby, self).__init__(**kwargs)
         self.dropdown = DropDown()
-        self.add_dropdown_content()
 
-    def add_dropdown_content(self):
+    def fetch_data(self):
+        # Clear existing widgets in dropdown
+        self.dropdown.clear_widgets()
         # Ajoutez du contenu au menu déroulant
-        btn1 = Button(text='Profil', size_hint_y=None, height=20, width= 200)
-        btn1.bind(on_release=lambda btn: self.on_dropdown_select("profil_user"))
-        self.dropdown.add_widget(btn1)
+        btn = Button(text='Profil', size_hint_y=None, height=20, width=500)
+        btn.bind(on_release=lambda btn: self.on_dropdown_select("profil_user"))
+        self.dropdown.add_widget(btn)
 
-        btn2 = Button(text='User home', size_hint_y=None, height=20, width= 200)
-        btn2.bind(on_release=lambda btn: self.on_dropdown_select("home_user"))
-        self.dropdown.add_widget(btn2)
+        # Django REST endpoint URL
+        endpoint_url = f'http://127.0.0.1:8000/baby/?expand=id_user&id_user={AppState.user_id}'
+        try:
+            # Make GET request to the endpoint
+            response = requests.get(endpoint_url)
+            response_data = response.json()
 
-        btn3 = Button(text='Profil Bébé', size_hint_y=None, height=20, width= 200)
-        btn3.bind(on_release=lambda btn: self.on_dropdown_select("babyprofil"))
-        self.dropdown.add_widget(btn3)
+            # Create a dynamic list based on the user ID
+            if response_data:
+                for baby in response_data:
+                    # Ajoutez du contenu au menu déroulant
+                    name_baby = baby.get('firstname', 'Unknown')
+                    btn = Button(text=f'{name_baby}', size_hint_y=None, height=20, width=500)
+                    btn.bind(on_release=lambda btn: self.on_dropdown_select("profil_user"))
+                    self.dropdown.add_widget(btn)
+        except Exception as e:
+            self.add_widget(Label(text=f"Error: {e}"))
+
+        btnlog = Button(text='Logout', size_hint_y=None, height=20, width=500)
+        btnlog.bind(on_release=lambda btn: self.on_dropdown_select("logout"))
+        self.dropdown.add_widget(btnlog)
 
     def on_dropdown_select(self, target):
         # Fonction appelée lorsque vous sélectionnez un bouton dans le menu déroulant
         if target == "profil_user":
             self.manager.current = "profil_user"
-        elif target == "home_user":
-            self.manager.current = "home_user"
-        elif target == "babyprofil":
-            self.manager.current = "babyprofil"
+        elif target == "logout":
+            response = requests.get('http://127.0.0.1:8000/logout/')
+            self.manager.current = "login"
 
     def open_dropdown(self, widget):
         # Ouvre le menu déroulant lorsque le bouton est relâché
+        self.fetch_data()  # Appeler fetch_data lorsque le bouton est pressé
         self.dropdown.open(widget)
 
-
-class Profil(Screen):
-    pass
-
-
-class Home_user(Screen):
-    pass
-
-class Profil_baby(Screen):
-    pass
 
 
 class babyhome(App):
--- client/home_user/home_user.py
+++ client/home_user/home_user.py
@@ -1,58 +1,50 @@
-#!/usr/bin/env python3
-
 import kivy
 from kivy.core.window import Window
 from kivy.app import App
 from client.login.Login import AppState
-from kivy.uix.recycleview import RecycleView
-from kivy.uix.screenmanager import ScreenManager, Screen
+from client.home_baby.home_baby import Home_baby
 from kivy.uix.carousel import Carousel
 from kivy.uix.label import Label
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 import requests
-
 
 Window.size = (430, 932)
 
 kivy.require('2.0.0')
 
 class Home_user(Screen):
-
     def __init__(self, **kwargs):
         super(Home_user, self).__init__(**kwargs)
         self.dropdown = DropDown()
-        self.add_dropdown_content()
 
-    def add_dropdown_content(self):
-
+    def fetch_data(self):
+        # Clear existing widgets in dropdown
+        self.dropdown.clear_widgets()
         # Ajoutez du contenu au menu déroulant
         btn = Button(text='Profil', size_hint_y=None, height=20, width=500)
         btn.bind(on_release=lambda btn: self.on_dropdown_select("profil_user"))
         self.dropdown.add_widget(btn)
 
         # Django REST endpoint URL
-        endpoint_url = f'http://127.0.0.1/baby/?expand=id_user&id_user={AppState.user_id}/'
+        endpoint_url = f'http://127.0.0.1:8000/baby/?expand=id_user&id_user={AppState.user_id}'
         try:
             # Make GET request to the endpoint
             response = requests.get(endpoint_url)
             response_data = response.json()
-            print(response_data)
 
             # Create a dynamic list based on the user ID
-            if response_data is not []:
-                for i in range(1, len(response_data)):
+            if response_data:
+                for baby in response_data:
                     # Ajoutez du contenu au menu déroulant
-                    name_baby = response_data[i]['firstname']
-                    btn1 = Button(text=f'{name_baby}', size_hint_y=None, height=20, width=500)
-                    btn1.bind(on_release=lambda btn: self.on_dropdown_select("profil_user"))
-                    self.dropdown.add_widget(btn1)
-            else:
-                self.add_widget(Label(text="No user ID found"))
+                    name_baby = baby.get('firstname', 'Unknown')
+                    btn = Button(text=f'{name_baby}', size_hint_y=None, height=20, width=500)
+                    btn.bind(on_release=lambda btn: self.on_dropdown_select("profil_user"))
+                    self.dropdown.add_widget(btn)
         except Exception as e:
             self.add_widget(Label(text=f"Error: {e}"))
-
-
 
         btnlog = Button(text='Logout', size_hint_y=None, height=20, width=500)
         btnlog.bind(on_release=lambda btn: self.on_dropdown_select("logout"))
@@ -68,25 +60,15 @@ class Home_user(Screen):
 
     def open_dropdown(self, widget):
         # Ouvre le menu déroulant lorsque le bouton est relâché
+        self.fetch_data()  # Appeler fetch_data lorsque le bouton est pressé
         self.dropdown.open(widget)
-
-
-class Profil(Screen):
-    pass
-
-
-class Home_baby(Screen):
-    pass
-
 
 class userhome(App):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(Home_user(name='home_user'))
-        sm.add_widget(Profil(name='profil_user'))
         sm.add_widget(Home_baby(name='babyhome'))
         return sm
-
 
 if __name__ == '__main__':
     userhome().run()
